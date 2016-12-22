adc002c01cec899b7b8915c0f0abdb528911426f
allengzh <18862242813@139.com>
2016-12-22 18:35:20 +0800

    加入pm2调度进程

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 /* Post pm add submit. */
 router.post('/pm/add', function(req, res, next) {
 
-	var data = [
-		'var Monitor = require(\'page-monitor\');',
-		'var num = 0;\nvar flag = 0;',
-		'function a() {',
-		'if (flag) {return;}',
-		'flag = 1;',
-		'var monitor = new Monitor(\''+req.body.url+'\', '+req.body.params+');',
-		'monitor.capture(function(code) {',
-		'console.log(\'case \' + num + \' done, success!\\n\');',
-		'flag = 0;\nnum++;',
-		'});',
-		'}',
-		'setInterval(a, '+req.body.time+');'
-	];
+    var data = [
+        'var Monitor = require(\'page-monitor\');',
+        'var num = 0;\nvar flag = 0;',
+        'function a() {',
+        'if (flag) {return;}',
+        'flag = 1;',
+        'var monitor = new Monitor(\'' + req.body.url + '\', ' + req.body.params + ');',
+        'monitor.capture(function(code) {',
+        'console.log(\'case \' + num + \' done, success!\\n\');',
+        'flag = 0;\nnum++;',
+        '});',
+        '}',
+        'setInterval(a, ' + req.body.time + ');'
+    ];
 
-	var text = data.join('\n');
-	var file = __dirname + '/../script/' + req.body.name + '.js';
+    var text = data.join('\n');
+    var file = __dirname + '/../script/' + req.body.name + '.js';
 
-	fs.writeFile(file, text, function(err) {
+    fs.writeFile(file, text, function(err) {
         if (err) {
             console.log(err);
-			res.json({ code: '0', info: '新增任务失败' })
+            res.json({ code: '1', info: '新建任务失败' })
         } else {
             console.log("params saved to " + file);
-            res.json({ code: '1', info: '新增任务成功' })
+
+            var cmdStr = 'pm2 start ' + file + ' --name ' + req.body.name;
+
+            exec(cmdStr, function(err, stdout, stderr) {
+                if (err) {
+                    console.log('start script error:' + stderr);
+                    res.json({ code: '2', info: '任务脚本执行失败' })
+                } else {
+                    res.json({ code: '0', info: '新增任务成功' })
+                }
+            });
         }
     });
-  // res.render('index', { title: 'Express' });
+    // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
