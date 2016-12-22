cafd3c5ce46635931099ad33f2d5e17d091f9168
allengzh <18862242813@139.com>
2016-12-22 17:35:46 +0800

    引入page-monitor，实现web化

---
+++ public/js/index.js
@@ -0,0 +1,69 @@
+function isJsonFormat(str) {
+    try {
+        eval('('+str+')');
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+$('#pm').on('click', '.am-text-primary', function() {
+    var parentNode = $(this).closest('tr');
+    var url = parentNode.find('.table-url').text();
+    var params = parentNode.find('.table-params').text();
+    var time = parentNode.find('.table-time').text();
+
+    $.ajax({
+        type: 'POST',
+        url: '/pm/start',
+        data: { phone: mobilephone, password: userPass },
+        dataType: 'json',
+        success: function(data) {
+            var code = parseInt(data.code);
+            var info = data.info;
+            if (code === 1) {
+                $ele.html('登录').removeAttr('disabled');
+                window.location.href = '/index';
+            } else {
+                $ele.html('登录').removeAttr('disabled');
+                showError(info);
+            }
+        }
+    });
+});
+
+$('#pm').on('click', '.add', function() {
+    $('#add-modal').modal({
+    	width: 640
+    });
+});
+
+$('#add-submit').on('click', function() {
+    var name = $.trim($('#add-name').val());
+    var desc = $.trim($('#add-desc').val());
+    var url = $.trim($('#add-url').val());
+    var params = $.trim($('#add-params').val());
+    var time = $.trim($('#add-time').val());
+
+    if (!isJsonFormat(params)) {
+        $('#add-params')[0].setCustomValidity('输入参数格式错误');
+    } else {
+        $('#add-params')[0].setCustomValidity('');
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/pm/add',
+        data: { name: name,desc: desc, url: url, params: params, time: time },
+        dataType: 'json',
+        success: function(data) {
+            var code = parseInt(data.code);
+            var info = data.info;
+            if (code === 1) {
+                
+            } else {
+                showError(info);
+            }
+        }
+    });
+})
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* Post pm add submit. */
+router.post('/pm/add', function(req, res, next) {
+
+	var data = [
+		'var Monitor = require(\'page-monitor\');',
+		'var num = 0;\nvar flag = 0;',
+		'function a() {',
+		'if (flag) {return;}',
+		'flag = 1;',
+		'var monitor = new Monitor(\''+req.body.url+'\', '+req.body.params+');',
+		'monitor.capture(function(code) {',
+		'console.log(\'case \' + num + \' done, success!\\n\');',
+		'flag = 0;\nnum++;',
+		'});',
+		'}',
+		'setInterval(a, '+req.body.time+');'
+	];
+
+	var text = data.join('\n');
+	var file = __dirname + '/../script/' + req.body.name + '.js';
+
+	fs.writeFile(file, text, function(err) {
+        if (err) {
+            console.log(err);
+			res.json({ code: '0', info: '新增任务失败' })
+        } else {
+            console.log("params saved to " + file);
+            res.json({ code: '1', info: '新增任务成功' })
+        }
+    });
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
