bf1282eed29faed3f09eab4190164679d9339228
allengzh <18862242813@139.com>
2016-12-23 18:16:29 +0800

    引入mongodb

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,39 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var exec = require('child_process').exec;
+var mongoose = require('mongoose');
+
+var uri = 'mongodb://localhost/pm';
+
+mongoose.connect(uri);
+
+//在Schma里定义数据类型
+var PmSchma = new mongoose.Schema({ //定义一个Schema
+    name: String,
+    url: String,
+    params: String,
+    time: Integer,
+    state: Integer,
+    mail: String
+});
+
+mongoose.model('Pm',PmSchma);
+
+var Pm = mongoose.model('Pm');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+
+	var data ={};
+
+	Pm.find({}, function (err,results) {
+        if(err){
+            console.log('error message',err);
+            return;
+        }
+        data = results;
+    });
+    res.render('index', { data: data });
 });
 
 /* Post pm add submit. */
@@ -32,20 +61,21 @@ router.post('/pm/add', function(req, res, next) {
     fs.writeFile(file, text, function(err) {
         if (err) {
             console.log(err);
-            res.json({ code: '1', info: '新建任务失败' })
+            res.json({ code: '1', info: '新建任务脚本失败' })
         } else {
+
             console.log("params saved to " + file);
 
-            var cmdStr = 'pm2 start ' + file + ' --name ' + req.body.name;
+            // var cmdStr = 'pm2 start ' + file + ' --name ' + req.body.name;
 
-            exec(cmdStr, function(err, stdout, stderr) {
-                if (err) {
-                    console.log('start script error:' + stderr);
-                    res.json({ code: '2', info: '任务脚本执行失败' })
-                } else {
-                    res.json({ code: '0', info: '新增任务成功' })
-                }
-            });
+            // exec(cmdStr, function(err, stdout, stderr) {
+            //     if (err) {
+            //         console.log('start script error:' + stderr);
+            //         res.json({ code: '2', info: '任务脚本执行失败' })
+            //     } else {
+            //         res.json({ code: '0', info: '新增任务成功' })
+            //     }
+            // });
         }
     });
     // res.render('index', { title: 'Express' });
