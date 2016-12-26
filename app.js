9105dece1d6d4982fa527e0bf2cf0f577318c3be
allengzh <18862242813@139.com>
2016-12-26 14:14:11 +0800

    表格优化

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression')
 
 var index = require('./routes/index');
 var app = express();
@@ -13,12 +14,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
+app.use(compression()); //use compression
 app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {etag: true}));
 
 app.use('/', index);
 
