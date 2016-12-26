85d2246cdbad177685bac2da3752bc2cc951ba73
allengzh <18862242813@139.com>
2016-12-26 15:28:47 +0800

    加上cdn

--- routes/index.js
+++ routes/index.js
@@ -18,63 +18,83 @@ var PmSchma = new mongoose.Schema({ //定义一个Schema
     mail: String
 }, { collection: 'pm' });
 
-mongoose.model('pm',PmSchma);
+mongoose.model('pm', PmSchma);
 
 var Pm = mongoose.model('pm');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	var data ={};
+    var data = {};
 
-	Pm.find({}, function (err,results) {
-        if(err){
-            console.log('error message',err);
+    Pm.findOne({}, function(err, results) {
+        if (err) {
+            console.log('error message', err);
             return;
         }
-    	res.render('index', { datas: results });
+        console.log(!results);
+        res.render('index', { datas: results });
     });
 });
 
 /* Post pm add submit. */
 router.post('/pm/add', function(req, res, next) {
 
-    var data = [
-        'var Monitor = require(\'page-monitor\');',
-        'var num = 0;\nvar flag = 0;',
-        'function a() {',
-        'if (flag) {return;}',
-        'flag = 1;',
-        'var monitor = new Monitor(\'' + req.body.url + '\', ' + req.body.params + ');',
-        'monitor.capture(function(code) {',
-        'console.log(\'case \' + num + \' done, success!\\n\');',
-        'flag = 0;\nnum++;',
-        '});',
-        '}',
-        'setInterval(a, ' + req.body.time + ');'
-    ];
-
-    var text = data.join('\n');
-    var file = __dirname + '/../script/' + req.body.name + '.js';
-
-    fs.writeFile(file, text, function(err) {
+    Pm.findOne({ name: req.body.name }, function(err, data) {
         if (err) {
-            console.log(err);
-            res.json({ code: '1', info: '新建任务脚本失败' })
+            console.log('error message', err);
+            return;
+        }
+
+        if (data) {
+            console.log('相同任务名已经存在');
+            res.json({ code: '1', info: '相同任务名已经存在' });
         } else {
+            var data = [
+                'var Monitor = require(\'page-monitor\');',
+                'var num = 0;\nvar flag = 0;',
+                'function a() {',
+                'if (flag) {return;}',
+                'flag = 1;',
+                'var monitor = new Monitor(\'' + req.body.url + '\', ' + req.body.params + ');',
+                'monitor.capture(function(code) {',
+                'console.log(\'case \' + num + \' done, success!\\n\');',
+                'flag = 0;\nnum++;',
+                '});',
+                '}',
+                'setInterval(a, ' + req.body.time + ');'
+            ];
 
-            console.log("params saved to " + file);
+            var text = data.join('\n');
+            var file = __dirname + '/../script/' + req.body.name + '.js';
 
-            // var cmdStr = 'pm2 start ' + file + ' --name ' + req.body.name;
+            fs.writeFile(file, text, function(err) {
+                if (err) {
+                    console.log(err);
+                    res.json({ code: '2', info: '新建任务脚本失败' })
+                } else {
+                    console.log("params saved to " + file);
 
-            // exec(cmdStr, function(err, stdout, stderr) {
-            //     if (err) {
-            //         console.log('start script error:' + stderr);
-            //         res.json({ code: '2', info: '任务脚本执行失败' })
-            //     } else {
-            //         res.json({ code: '0', info: '新增任务成功' })
-            //     }
-            // });
+                    var insertData = {name: req.body.name, url: req.body.url, params: req.body.params, time: req.body.time, state: 0, mail: req.body.mail};
+
+                    Pm.create(insertData, function(err) {
+                        if (err) {
+                            res.json({ code: '3', info: '入库失败' });
+                        }else{
+                        	var cmdStr = 'pm2 start ' + file + ' --name ' + req.body.name;
+
+		                    exec(cmdStr, function(err, stdout, stderr) {
+		                        if (err) {
+		                            console.log('start script error:' + stderr);
+		                            res.json({ code: '4', info: '任务脚本执行失败' })
+		                        } else {
+		                            res.json({ code: '0', info: '新增任务成功', data: insertData })
+		                        }
+		                    });
+                        }
+                    });      
+                }
+            });
         }
     });
 });
