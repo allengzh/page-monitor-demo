eb97757b4e1b5000be5fa1532b03763f56b0a9c4
allengzh <18862242813@139.com>
2017-04-14 14:59:16 +0800

    后端加上校验

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,20 @@ router.get('/', function(req, res, next) {
 
 /* Post pm add submit. */
 router.post('/pm/add', function(req, res, next) {
+    if(/^[A-Za-z]+$/.test(req.body.name)){
+        res.json({ code: 4, info: '请输入任务名称，必须由字母组成' });
+    }
+    if(/^(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?$/.test(req.body.name)){
+        res.json({ code: 5, info: '请输入正确格式的网页链接' });
+    }
+    try {
+        JSON.parse(req.body.params);
+    } catch (e) {
+        res.json({ code: 6, info: '请输入正确格式的参数' });
+    }
+    if(/^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(req.body.time)){
+        res.json({ code: 7, info: '请输入正确格式的数字' });
+    }
 
     Pm.findOne({ name: req.body.name }, function(err, data) {
         if (err) {
@@ -79,6 +93,21 @@ router.post('/pm/add', function(req, res, next) {
 
 /* Post pm edit submit. */
 router.post('/pm/update', function(req, res, next) {
+
+    if(/^[A-Za-z]+$/.test(req.body.name)){
+        res.json({ code: 4, info: '请输入任务名称，必须由字母组成' });
+    }
+    if(/^(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?$/.test(req.body.name)){
+        res.json({ code: 5, info: '请输入正确格式的网页链接' });
+    }
+    try {
+        JSON.parse(req.body.params);
+    } catch (e) {
+        res.json({ code: 6, info: '请输入正确格式的参数' });
+    }
+    if(/^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(req.body.time)){
+        res.json({ code: 7, info: '请输入正确格式的数字' });
+    }
 
     Pm.findOne({ name: req.body.name }, function(err, data) {
         if (err) {
