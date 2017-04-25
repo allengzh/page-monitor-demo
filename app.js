4a037fbfc2f9e6d3e11d5692ea4f4160ed88dc41
allengzh <18862242813@139.com>
2017-04-25 13:51:36 +0800

    引入告警邮件

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'), {etag: true}));
+app.use(express.static(path.join(__dirname, 'output'), {etag: true}));
 
 app.use('/', index);
 
